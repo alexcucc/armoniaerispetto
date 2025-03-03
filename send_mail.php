91860de8b6c7321f9e77d3f6e993b2f0bd3d4268
Alessandro Cucco <alessandro.cucco@weavr.io>
2025-03-03 19:15:17 +0100

    test send mail

--- send_mail.php
+++ send_mail.php
@@ -1,52 +1,39 @@
-<?php 
+<?php
 
-    error_reporting(E_ALL);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = trim(filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING));
+    $email = trim(filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL));
+    $message = trim(filter_input(INPUT_POST, "message", FILTER_SANITIZE_STRING));
 
-        $to = "alex.cucc@hotmail.it";
-        $email = $_POST['email'];
-        $name = $_POST['name'];
-        $phone = $_POST['phone'];
-        $message = $_POST['message'];
-        $sender = "postmaster@armoniaerispetto.it";
-        $returnPath = "-f$sender";
-        $subjectOperator = "Richiesta informazioni";
-        $subjectCustomer = "Grazie per la tua email, ti ricontatteremo a breve";
-        $messageOperator = "Nome: $name \nE-mail: $email\nTelefono: $phone\nMessaggio: $message";
-        $messageCustomer = "$name grazie per la tua email, ti ricontatteremo a breve";
-        $headersOperator = "From: $email";
-        $headersCustomer = "From: $to";
+    if (empty($name) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
-        sendMailOrThrowError(
-            $to,
-            $subjectOperator,
-            $messageOperator,
-            $headersOperator,
-            $returnPath);
-        /*sendMailOrThrowError(
-            $email,
-            $subjectCustomer,
-            $messageCustomer,
-            $headersCustomer,
-            $returnPath);*/
+        http_response_code(400);
+        echo "Please fill in all fields and provide a valid email address.";
+        exit;
     }
 
-    function sendMailOrThrowError(
-        $to,
-        $subject,
-        $message,
-        $headers,
-        $returnPath) {
+    $to = "alex.cucc@hotmail.it";
+    
+    $subject = "New contact from $name";
 
-            if (!mail(
-                $to,
-                $subject,
-                $message,
-                $headers,
-                $returnPath)) {
-                    error_log("Error while sending email to: " . $to);
-                    http_response_code(500);
-                }
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    $headers = "From: $name <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    if (mail($to, $subject, $email_content, $headers)) {
+
+        http_response_code(200);
+    } else {
+
+        http_response_code(500);
     }
+} else {
+
+    http_response_code(403);
+}
+
 ?>
