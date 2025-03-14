94a2e18d1967b91ea8ed2cbf0fc71b53b1ae01e9
Alessandro Cucco <alessandro.cucco@weavr.io>
2025-03-14 18:50:47 +0100

    added icon

--- common-head.php
+++ common-head.php
@@ -2,3 +2,4 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <link rel="stylesheet" href="styles.css"/>
+<link rel="icon" type="image/x-icon" href="/images/favicon.ico">
