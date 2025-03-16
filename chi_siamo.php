e470c4584420af6bf2aa95eb2bf469c91b2350d7
Alessandro Cucco <alessandro.cucco@weavr.io>
2025-03-16 22:52:32 +0100

    added text

--- chi_siamo.php
+++ chi_siamo.php
@@ -18,7 +18,7 @@
           </div>
           <div class="content-right">
             <h2>Trasformiamo in cura la nostra forza</h2>
-            La fondazione “Armonia e Rispetto” nasce nel 2024 per iniziativa di un imprenditore e di un gruppo di entusiasti sostenitori che condividono la passione per gli animali e la convinzione che ad ogni essere vivente sia dovuto un rispetto speciale. Il rispetto che si deve portare verso chi, davanti all'uomo, è del tutto indifeso e spesso destinato a soccombere e persino ad estinguersi come specie.<br>
+            La fondazione “Armonia e Rispetto” è una fondazione di erogazione che nasce nel 2024 per iniziativa di un imprenditore e di un gruppo di entusiasti sostenitori che condividono la passione per gli animali e la convinzione che ad ogni essere vivente sia dovuto un rispetto speciale. Il rispetto che si deve portare verso chi, davanti all'uomo, è del tutto indifeso e spesso destinato a soccombere e persino ad estinguersi come specie.<br>
             <br>
             Lavoriamo per evitare che il più forte, per i suoi fini travolga e distrugga i nostri amici più deboli.
             <br>
