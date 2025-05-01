f05e04ae3600dfb9a2216ab4aa40b293251f2b5e
Alessandro Cucco <alessandro.cucco@weavr.io>
2025-05-01 09:56:19 +0200

    Refactor PDF display logic in statuto.php for improved mobile compatibility; streamline code structure

--- statuto.php
+++ statuto.php
@@ -14,35 +14,24 @@
         <div class="content-container">
           <div class="content">
             <div class="pdf-container">
-            <?php
-              $userAgent = $_SERVER['HTTP_USER_AGENT'];
-              $isMobile = preg_match('/(android|iphone|ipad|mobile)/i', $userAgent);
-              
-              if ($isMobile) {
-                  echo '<div class="mobile-pdf-notice">
-                          <div class="pdf-options">
-                              <a href="documents/statuto.pdf" class="pdf-button" download>
-                                  <span class="icon">📥</span>
-                                  Scarica PDF
-                              </a>
-                              <a href="documents/statuto.pdf" class="pdf-button" target="_blank">
-                                  <span class="icon">👁️</span>
-                                  Visualizza PDF
-                              </a>
-                          </div>
-                        </div>';
-              } else {
-                  echo '<object
-                          data="documents/statuto.pdf"
-                          type="application/pdf"
-                          width="100%"
-                          height="100%">
-                          <p>Il tuo browser non supporta la visualizzazione PDF.
-                             <a href="documents/statuto.pdf" download>Scarica il PDF</a>
-                          </p>
-                        </object>';
-              }
-              ?>
+              <object
+                data="documents/statuto.pdf"
+                type="application/pdf"
+                width="100%"
+                height="100%">
+                <div class="mobile-pdf-notice">
+                  <div class="pdf-options">
+                      <a href="documents/statuto.pdf" class="pdf-button" download>
+                          <span class="icon">📥</span>
+                          Scarica PDF
+                      </a>
+                      <a href="documents/statuto.pdf" class="pdf-button" target="_blank">
+                          <span class="icon">👁️</span>
+                          Visualizza PDF
+                      </a>
+                  </div>
+                </div>
+              </object>          
             </div>
           </div>
         </div>
