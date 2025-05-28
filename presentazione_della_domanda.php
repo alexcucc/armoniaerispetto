7266558eba726e71e3714bbf5f81acba13c66389
Alessandro Cucco <alessandro.cucco@weavr.io>
2025-05-28 19:19:41 +0200

    Enhance user instructions and download link visibility in 'presentazione_della_domanda.php'

--- presentazione_della_domanda.php
+++ presentazione_della_domanda.php
@@ -12,19 +12,24 @@
           <h1>Presentazione della Domanda</h1>
         </div>
         <div class="content-container">
-          <?php
-          if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
-          ?>
-          <a href="documents/presentazione_domanda_bando.zip" class="page-button" download>
-            Scarica file
-          </a>
-          <?php
-          } else {
-          ?>
-            <p>Per accedere a questa sezione, effettua il <a href="login.php">login</a> o <a href="signup.php">registrati</a>.</p>
+          <div class="content">
             <?php
-          }
-          ?>
+            if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
+            ?>
+            <p>Per rispondere al bando e presentare una domanda di finanziamento è  necessario scaricare e compilare i seguenti 3 Form e prendere visione delle "Linee guida alla rendicontazione"</p>
+            <p>I tre moduli compilati dovranno essere inviati a <a href="mailto:segreteria@armoniaerispetto.it">segreteria@armoniaerispetto.it</a> assieme ad eventuali vostre integrazioni o proposte.</p>
+            <p>Sempre allo stesso indirizzo email è possibile indirizzare richieste di informazioni o chiarimenti sul bando e su come parteciparvi.</p>
+            <a href="documents/presentazione_domanda_bando.zip" class="page-button" download>
+              Scarica file
+            </a>
+            <?php
+            } else {
+            ?>
+              <p>Per accedere a questa sezione, effettua il <a href="login.php">login</a> o <a href="signup.php">registrati</a>.</p>
+              <?php
+            }
+            ?>
+          </div>
         </div>
       </div>
     </main>
