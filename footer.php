aa1684950ff144121253d922c7b36a6fa1f53d52
Alessandro Cucco <alessandro.cucco@weavr.io>
2025-06-08 20:36:09 +0200

    Add email link to footer for improved contact accessibility

--- footer.php
+++ footer.php
@@ -5,6 +5,8 @@
             FONDAZIONE ARMONIA E RISPETTO ETS
             <br>
             Iscritta al Registro Unico del Terzo Settore Atto DD 1104/A2202A/2024 - Codice Fiscale 94090600019
+            <br>
+            <a href="mailto:info@armoniaerispetto.it">info@armoniaerispetto.it</a>
         </div>
     </div>
 </footer> 
