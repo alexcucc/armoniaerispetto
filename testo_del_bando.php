05a82f8c3df25079fdd08f0831c7fa77f1cb7036
Alessandro Cucco <alessandro.cucco@weavr.io>
2025-06-19 18:44:10 +0200

    Refactor button layout for improved accessibility and consistency in 'testo_del_bando.php' and enhance button styling in 'styles.css'

--- testo_del_bando.php
+++ testo_del_bando.php
@@ -19,10 +19,14 @@
               class="pdf-viewer">
               <p>Il tuo browser non supporta la visualizzazione di PDF. Puoi scaricare il file usando il pulsante qui sotto.</p>
             </object>
-            <a href="documents/bando.pdf" class="page-button" download>
-              Scarica il bando
-            </a>
-            <button onclick="window.location.href='presentazione_della_domanda.php';" class="page-button">Presentazione della domanda</button>
+            <div class="button-container">
+              <a href="documents/bando.pdf" class="page-button" download>
+                Scarica il bando
+              </a>
+              <button onclick="window.location.href='presentazione_della_domanda.php';" class="page-button">
+                Presentazione della domanda
+              </button>
+            </div>
           </div>
         </div>
       </div>
