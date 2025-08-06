48278f9989f89f0f031d63d96ef77dfb323cc0d9
Alessandro Cucco <alessandro.cucco@weavr.io>
2025-08-06 21:11:39 +0200

    updated formatting in valori.php

--- valori.php
+++ valori.php
@@ -12,80 +12,98 @@
           <h1>I nostri Valori</h1>
         </div>
         <div class="content-container">
-          <div class="content">
-            <img class="image" src="images/woman_earth.jpg">
-          </div>
-          <div class="content">
-            <p>Viviamo su un pianeta abitato da oltre 8 miliardi di esseri umani e più di 100 miliardi di animali vertebrati, domestici e selvatici.</p>
-            <p><b>L'uomo è la specie più intelligente, ma crediamo che la vera intelligenza risieda nella responsabilità, non nel dominio.</b></p>
-          </div>
-        </div>
-        <div class="content-container">
-          <div class="content">
-            <p><b>Ogni essere vivente ha un valore intrinseco, indipendentemente dalla sua utilità per l'uomo.</b></p>
-            <p>Lottiamo per un mondo in cui nessun animale venga sfruttato, abusato o trattato come oggetto. Ci impegniamo in campagne, azioni legali e progetti educativi per abolire ogni forma di violenza e sfruttamento, agendo a tutela dei diritti animali.</p>
-          </div>
-          <div class="content">
-            <img class="image" src="images/sunflower.jpg">
+          <div class="overlay-wrapper">
+            <div class="overlay-image">
+              <img class="image fade-right" src="images/woman_earth.jpg">
+            </div>
+            <div class="overlay-text-right">
+              <p>Viviamo su un pianeta abitato da oltre 8 miliardi di esseri umani e più di 100 miliardi di animali vertebrati, domestici e selvatici.​</p>
+              <p><b>L'uomo è la specie più intelligente, ma crediamo che la vera intelligenza risieda nella responsabilità, non nel dominio.​</b></p>
+            </div>
           </div>
         </div>
         <div class="content-container">
-          <div class="content">
-            <img class="image" src="images/kangaroo_fire.jpg">
-          </div>
-          <div class="content">
-            <p><b>Difendere chi non ha voce è il primo passo verso una civiltà più giusta.</b></p>
-          </div>
-        </div>
-        <div class="content-container">
-          <div class="content">
-            <p>Dove l'uomo distrugge, la natura tenta di rinascere: ma non può farlo da sola. <b>Le nostre azioni quotidiane sono semi</b>: da esse può nascere un mondo più gentile.</p>
-            <p>Ogni nostra scelta quotidiana può diventare un atto di difesa per il pianeta e chi lo abita.</p>
-            <p>Promuoviamo stili di vita consapevoli, dall'alimentazione all'uso delle risorse, per incentivare comportamenti concreti a tutela dell'ambiente.</p>
-          </div>
-          <div class="content">
-            <img class="image" src="images/earth_leaves.jpg">
+          <div class="overlay-wrapper">
+            <div class="overlay-text-left">
+              <p><b>Ogni essere vivente ha un valore intrinseco, indipendentemente dalla sua utilità per l'uomo.</b></p>
+              <p>Lottiamo per un mondo in cui nessun animale venga sfruttato, abusato o trattato come oggetto. Ci impegniamo in campagne, azioni legali e progetti educativi per abolire ogni forma di violenza e sfruttamento, agendo a tutela dei diritti animali.​</p>
+            </div>
+            <div class="overlay-image">
+              <img class="image fade-left" src="images/sunflower.jpg">
+            </div>
           </div>
         </div>
         <div class="content-container">
-          <div class="content">
-            <img class="image" src="images/mountain_falcon.jpg">
-          </div>
-          <div class="content">
-            <p>Crediamo in una convivenza che non si limiti alla tolleranza, ma coltivi rispetto e protezione reciproca.</p>
-            <p>Agiamo secondo la visione "One Health", riconoscendo che il benessere umano è legato in modo profondo a quello della natura e degli animali.</p>
-          </div>
-        </div>
-        <div class="content-container">
-          <div class="content">
-            <p><b>Ogni foresta abbattuta, ogni specie estinta, ci allontana da ciò che siamo veramente.</b></p>
-            <p>Sosteniamo iniziative volte alla tutela delle specificità naturali di ogni luogo, alla conservazione della biodiversità e alla resilienza climatica.</p>
-          </div>
-          <div class="content">
-            <img class="image" src="images/jaguar.jpg">
+          <div class="overlay-wrapper">
+            <div class="overlay-image">
+              <img class="image fade-right" src="images/kangaroo_fire.jpg">
+            </div>
+            <div class="overlay-text-right">
+              <p><b>Difendere chi non ha voce è il primo passo verso una civiltà più giusta.</b></p>
+              <p>Dare voce agli animali, alla natura e a chi non può chiedere aiuto significa riconoscere che ogni vita ha valore. È un atto di responsabilità, di empatia ma soprattutto di evoluzione culturale.</p>
+              <p>Nel silenzio degli innocenti nasce la nostra coscienza e solo ascoltando quel silenzio possiamo chiamarci umani.</p>
+            </div>
           </div>
         </div>
         <div class="content-container">
-          <div class="content">
-            <img class="image" src="images/family_tree.jpg">
-          </div>
-          <div class="content">
-            <p>Promuoviamo un'ecologia concreta, locale, fatta di scelte consapevoli e azioni condivise.</p>
-            <p>Ogni albero, ogni animale selvatico, ogni ruscello è parte della nostra stessa storia. La Fondazione lavora affinché la memoria ecologica venga custodita e trasmessa, anche tramite il recupero di aree naturali marginalizzate.</p>
+          <div class="overlay-wrapper">
+            <div class="overlay-text-left">
+              <p>Dove l’uomo distrugge, la natura tenta di rinascere: ma non può farlo da sola. <b>Le nostre azioni quotidiane sono semi:</b> da esse può nascere un mondo più gentile.</p>
+              <p>Ogni nostra scelta quotidiana può diventare un atto di difesa per il pianeta e chi lo abita.​</p>
+              <p>Promuoviamo stili di vita consapevoli, dall’alimentazione all’uso delle risorse, per incentivare comportamenti concreti a tutela dell’ambiente.</p>
+            </div>
+            <div class="overlay-image">
+              <img class="image fade-left" src="images/earth_leaves.jpg">
+            </div>
           </div>
         </div>
         <div class="content-container">
-          <div class="content">
-            <p><b>Non vogliamo salvare il mondo da soli, vogliamo farlo insieme: uomini, animali e natura, finalmente in Armonia.</b></p>
+          <div class="overlay-wrapper">
+            <div class="overlay-image">
+              <img class="image fade-right" src="images/mountain_falcon.jpg">
+            </div>
+            <div class="overlay-text-right">
+              <p><b>Crediamo in una convivenza che</b> non si limiti alla tolleranza, ma <b>coltivi rispetto e protezione reciproca.</b></p>
+              <p>Agiamo secondo la visione "One Health", riconoscendo che il benessere umano è legato in modo profondo a quello della natura e degli animali.​</p>
+            </div>
           </div>
-          <div class="content">
-            <img class="image" src="images/man_dog_sunset.jpg">
+        </div>
+        <div class="content-container">
+          <div class="overlay-wrapper">
+            <div class="overlay-text-left">
+              <p><b>Ogni foresta abbattuta, ogni specie estinta, ci allontana da ciò che siamo veramente.</b></p>
+              <p>Sosteniamo iniziative volte alla tutela delle specificità naturali di ogni luogo, alla conservazione della biodiversità e alla resilienza climatica.​</p>
+            </div>
+            <div class="overlay-image">
+              <img class="image fade-left" src="images/jaguar.jpg">
+            </div>
+          </div>
+        </div>
+        <div class="content-container">
+          <div class="overlay-wrapper">
+            <div class="overlay-image">
+              <img class="image fade-right" src="images/family_tree.jpg">
+            </div>
+            <div class="overlay-text-right">
+              <p><b>Promuoviamo un'ecologia concreta, locale, fatta di scelte consapevoli e azioni condivise.</b></p>
+              <p>Ogni albero, ogni animale selvatico, ogni ruscello è parte della nostra stessa storia.     La Fondazione lavora affinché la memoria ecologica venga custodita e trasmessa, anche tramite il recupero di aree naturali marginalizzate.</p>
+            </div>
+          </div>
+        </div>
+        <div class="content-container">
+          <div class="overlay-wrapper">
+            <div class="overlay-text-left">
+              <p><b>Non vogliamo salvare il mondo da soli, vogliamo farlo insieme: uomini, animali e natura, finalmente in Armonia</b> camminando fianco a fianco verso un futuro condiviso, dove <b>ogni battito ogni respiro e ogni radice contano,</b> e la Terra non è più divisa in padroni e vittime, ma unita in un'unica voce che chiede rispetto cura e bellezza.</p>
+            </div>
+            <div class="overlay-image">
+              <img class="image fade-left" src="images/man_dog_sunset.jpg">
+            </div>
           </div>
         </div>
         <div class="content-container">
           <div class="content">
             <p>Il nostro futuro dipende dalla capacità di convivere in equilibrio con la natura. Favoriamo modelli di coabitazione tra uomo e fauna selvatica, basati sul rispetto, sulla prevenzione dei conflitti e sulla valorizzazione reciproca.</p>
-            <p>Operiamo in prima linea o sostenendo chi già agisce, perché la difesa della vita è una rete.</p>
+            <p><b>Operiamo in prima linea o sostenendo chi già agisce, perché la difesa della vita è una rete.</b></p>
             <p>Collaboriamo con enti, istituzioni e associazioni che condividono i nostri scopi, creando sinergie per moltiplicare l'impatto positivo.</p>
             <p>Crediamo che cambiare il mondo sia possibile, a partire da scelte consapevoli e dalla bellezza della cura. <b>Ogni piccolo gesto conta. Per questo diffondiamo la cultura della responsabilità, della bellezza e dell'impegno verso un mondo più giusto per tutti gli esseri viventi.</b></p>
           </div>
