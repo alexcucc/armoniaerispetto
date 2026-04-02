82c9be88200d7ef1362c84e1cf24106c9d58958e
Alessandro Cucco <alessandro.cucco@weavr.io>
2026-04-02 18:15:38 +0200

    devs

--- db/migrations/V36__user_default_call_for_proposal.sql
+++ db/migrations/V36__user_default_call_for_proposal.sql
@@ -1,9 +1,11 @@
-ALTER TABLE user
-ADD COLUMN default_call_for_proposal_id INT NULL;
+ALTER TABLE `user`
+ADD COLUMN `default_call_for_proposal_id` INT NULL;
 
-ALTER TABLE user
-ADD CONSTRAINT fk_user_default_call_for_proposal
-FOREIGN KEY (default_call_for_proposal_id) REFERENCES call_for_proposal(id) ON DELETE SET NULL;
+ALTER TABLE `user`
+ADD CONSTRAINT `fk_user_default_call_for_proposal`
+FOREIGN KEY (`default_call_for_proposal_id`)
+REFERENCES `call_for_proposal`(`id`)
+ON DELETE SET NULL;
 
-CREATE INDEX idx_user_default_call_for_proposal_id
-ON user (default_call_for_proposal_id);
+CREATE INDEX `idx_user_default_call_for_proposal_id`
+ON `user` (`default_call_for_proposal_id`);
